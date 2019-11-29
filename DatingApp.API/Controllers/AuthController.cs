a234405391cf7435f787d3148c28f1305621261c
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-11-29 10:06:52 -0600

    Generating JWT in AuthController

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
@@ -11,10 +17,12 @@ namespace DatingApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repository;
+        private readonly IConfiguration _configuration;
 
-        public AuthController(IAuthRepository repository)
+        public AuthController(IAuthRepository repository, IConfiguration configuration)
         {
             _repository = repository;
+            _configuration = configuration;
         }
 
         [HttpPost("register")]
@@ -22,7 +30,7 @@ namespace DatingApp.API.Controllers
         {
             // validating request happening in DTO while casting from POST request by [APIController]
             // all validating error taking care by [APIController]
-            
+
             userForRegisterDto.UserName = userForRegisterDto.UserName.ToLower();
 
             if (await _repository.UserExist(userForRegisterDto.UserName)) return BadRequest("Username already exist");
@@ -35,6 +43,54 @@ namespace DatingApp.API.Controllers
             var createUser = await _repository.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepository =
+                await _repository.Login(userForLoginDto.UserName.ToLower(), userForLoginDto.Password);
+            if (userFromRepository == null) return Unauthorized();
+
+            var (tokenHandler, token) = GetTokenAndTokenHandler(userFromRepository);
+            
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token)
+            });
+        }
+
+        public Tuple<JwtSecurityTokenHandler, SecurityToken> GetTokenAndTokenHandler(User userFromRepository)
+        {
+//            var claims = new[]
+//            {
+//                new Claim(ClaimTypes.NameIdentifier, userFromRepository.Id.ToString()),
+//                new Claim(ClaimTypes.Name, userFromRepository.UserName),
+//            };
+
+            var securityKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value)
+            );
+
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+
+            // start creating token
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userFromRepository.Id.ToString()),
+                    new Claim(ClaimTypes.Name, userFromRepository.UserName),
+                }),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = signingCredentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            
+            return new Tuple<JwtSecurityTokenHandler, SecurityToken>(tokenHandler, token);
         }
     }
 }
